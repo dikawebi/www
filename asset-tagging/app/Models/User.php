a372cd15cef4b900b9e2a6f680cde8af69f5a2e4
Andika Kuswidyarto <andika.kuswidyarto@gmail.com>
2026-06-08 10:44:04 +0700

    fix: implement FilamentUser interface in User model

--- asset-tagging/app/Models/User.php
+++ asset-tagging/app/Models/User.php
@@ -10,11 +10,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Filament\Panel;
+use Filament\Models\Contracts\FilamentUser;
 //use Filament\Models\Contracts\FilamentUser;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
