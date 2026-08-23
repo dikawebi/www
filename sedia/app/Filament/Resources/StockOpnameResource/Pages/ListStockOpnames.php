bbb781a90381c4b67dccb8a425772dce6e70565a
Andika Kuswidyarto <andika.kuswidyarto@gmail.com>
2026-08-23 23:30:22 +0700

    feat: add CreateAction to ListStockOpnames and ListStockTransfers pages

--- sedia/app/Filament/Resources/StockOpnameResource/Pages/ListStockOpnames.php
+++ sedia/app/Filament/Resources/StockOpnameResource/Pages/ListStockOpnames.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\StockOpnameResource\Pages;
 
 use App\Filament\Resources\StockOpnameResource;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListStockOpnames extends ListRecords
 {
     protected static string $resource = StockOpnameResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
 }
--- sedia/app/Filament/Resources/StockTransferResource/Pages/ListStockTransfers.php
+++ sedia/app/Filament/Resources/StockTransferResource/Pages/ListStockTransfers.php
@@ -3,9 +3,17 @@
 namespace App\Filament\Resources\StockTransferResource\Pages;
 
 use App\Filament\Resources\StockTransferResource;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListStockTransfers extends ListRecords
 {
     protected static string $resource = StockTransferResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
 }
